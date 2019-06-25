c193519aa994ad7d283371278ba7e0aaff60bd77
Pascal Weiß <pa-weiss@gmx.de>
2019-06-25 14:53:17 +0200

    Added a test_receiver for logging the messages
    The test_receiver receives the messages from the generator and logs them.

--- message_generator/test_receiver.py
+++ message_generator/test_receiver.py
@@ -1,10 +1,15 @@
+from commons import *
 from kafka import KafkaConsumer
-from os import environ
 from config import *
+from logging import info
 
 if __name__ == '__main__':
+    prepare_logging(LOG_FILE_PATH, LOG_FORMAT, LOG_LEVEL)
+    info('Setting: \n ' + get_env_variable_str(locals()))
+
     bootstrap_servers = ['{}:{}'.format(KAFKA_BROKER_HOST, KAFKA_BROKER_PORT)]
     consumer = KafkaConsumer(KAFKA_TARGET_TOPIC, bootstrap_servers=bootstrap_servers)
+    info('\nStart receiving......')
     for msg in consumer:
-        print(msg)
+        info(msg)
 

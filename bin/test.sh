c835458f4c35fecc8a74dc71fe8385fa041f76e7
Pascal Weiß <pa-weiss@gmx.de>
2019-07-03 14:17:24 +0200

    Added longer time period for startup of kafka/zookeeper
    Reason: Tests may fail, if kafka/zookeeper startup has not finished

--- bin/test.sh
+++ bin/test.sh
@@ -4,6 +4,6 @@ set -e
 
 echo "\nPlease ensure to execute 'source environ.sh'\n"
 docker-compose -f ${TEST_MESSAGE_GENERATOR_PATH}/tests/resources/docker-compose.yml up -d
-sleep 5
+sleep 8
 py.test --capture no tests
 docker-compose -f ${TEST_MESSAGE_GENERATOR_PATH}/tests/resources/docker-compose.yml down

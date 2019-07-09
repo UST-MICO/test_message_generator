fa07cd2ebe7b4cef0e31e9d0095ff5c2185ea299
David Kopp <david@kopp-heim.de>
2019-07-09 15:41:55 +0200

    Fix echo in test script

--- bin/test.sh
+++ bin/test.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-echo "\nPlease ensure to execute 'source environ.sh'\n"
+echo -e "\nPlease ensure to execute 'source environ.sh'\n"
 docker-compose -f ${TEST_MESSAGE_GENERATOR_PATH}/tests/resources/docker-compose.yml up -d
 sleep 8
 py.test --capture no tests
